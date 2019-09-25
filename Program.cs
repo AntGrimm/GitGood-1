64f2c7ddc06f97179f0eaadf0242dbdbe26f848c
Mark Dewey <mark.t.dewey@gmail.com>
2019-09-25 11:13:17 -0400

    added some cool features

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
 
 namespace GitGood
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("Seeeeeeeeearch");
     }
+  }
 }
